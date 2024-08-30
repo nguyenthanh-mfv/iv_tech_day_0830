1c92a7f0b0f42ebba3ddcc70478bee7e97a24dac
Thanh Nguyen <nguyen.thanh@moneyforward.co.jp>
2024-08-30 14:50:06 +0700

    show seats and user_seats

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -1,5 +1,9 @@
 class SeatsController < ApplicationController
   def index
+    from = params[:from] || DateTime.current.beginning_of_day + 8.hours
+
+    @seats = Seat.order(number: :asc)
+    @user_seats = UserSeat.where(from: from..).index_by(&:seat_id)
   end
 
   def create
